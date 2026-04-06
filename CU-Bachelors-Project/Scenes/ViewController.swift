ac1185127c36ea4bba61530233f5ab35f59ecc81
nnttl <nataliizhgenti@gmail.com>
2026-04-06 18:33:52 +0400

    Initial commit

---
+++ CU-Bachelors-Project/Scenes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CU-Bachelors-Project
+//
+//  Created by Natali Zhgenti on 06.04.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
